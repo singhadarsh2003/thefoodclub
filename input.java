7d892e5848604ff10f9d9e19d8edb28237ce29a4
singhadarsh2003 <as2823353@gmail.com>
2024-03-14 18:53:25 -0700

    add first

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int number = i.nextInt();
+        String ii = i.next();
+        String iii = i.nextLine();
+        System.err.println("your entered number is :"+ number);
+        System.err.println("your entered number is :"+ ii);
+        System.err.println("your entered number is :"+ iii);
+
+
+    }
+}
